692a5688a1520a5e5b300c0a3374173d058b0728
App Academy Student <student@appacademy.io>
2016-10-05 09:52:27 -0700

    added skeleton to users model

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+  before_validation :ensure_session_token
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: true, message: "Password can't be blank"
+  validates :password_digest, length: { minimum: 6, }, allow_nil: true
+
+  def self.find_by_credentials
+    user = User.find_by(username: username)
+
+  end
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token!
+
+  end
+
+  def ensure_session_token
+
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
